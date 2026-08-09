f5449ec2314df9ce49b6f476894b52c27d469e7c
moslemsasan0-hue <moslemsasan0@gmail.com>
2026-08-09 11:13:29 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "Starting Xray..."
-echo "Xray will listen on 0.0.0.0:8443"
+echo "Starting Nginx..."
+nginx -t
+nginx
 
+echo "Starting Xray..."
 exec xray run -config /etc/xray/config.json
